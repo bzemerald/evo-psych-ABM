60344e35972e1d3852e9d101147a7dfee9094837
bzemerald <wym1145141919@gmail.com>
2025-11-27 18:46:09 -0600

    added changing color for agents

--- app.py
+++ app.py
@@ -1,13 +1,63 @@
 from model import Sugarscape
 from mesa.visualization import Slider, SolaraViz, SpaceRenderer, make_plot_component
 from mesa.visualization.components import AgentPortrayalStyle, PropertyLayerStyle
+RED = "#ff0000"
+GREEN = "#00ff00"
+BLUE = "#0000ff"
+ORANGE = "#ffa500"
+PURPLE = "#800080"
+CYAN = "#00ffff"
+MAGENTA = "#ff00ff"
+YELLOW = "#ffff00"
+BLACK = "#000000"
+WHITE = "#ffffff"
+GRAY = "#808080"
+LIGHT_GRAY = "#d3d3d3"
+DARK_GRAY = "#404040"
+
+
+def hex_to_rgb(color: str):
+    color = color.lstrip("#")
+    return tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(*rgb)
+
+def lerp(a, b, t: float):
+    return a + (b - a) * t
+
+def map_color(t: float, color1: str, color2: str) -> str:
+    """
+    t 在 [0, 1] 之间，把 t 映射到 color1 和 color2 之间的渐变色。
+    color1, color2 是 '#RRGGBB' 格式的字符串。
+    """
+    # 限制在 [0, 1]
+    t = max(0.0, min(1.0, t))
+
+    r1, g1, b1 = hex_to_rgb(color1)
+    r2, g2, b2 = hex_to_rgb(color2)
+
+    r = int(lerp(r1, r2, t))
+    g = int(lerp(g1, g2, t))
+    b = int(lerp(b1, b2, t))
+
+    return rgb_to_hex((r, g, b))
+
+
+def num_to_color(num: float, low, high, low_c=RED, high_c=GREEN) -> str:
+    """
+    Map an float value to a color between low_c and high_c. 
+    """
+    clamped = max(low, min(high, num))
+    t = clamped / (high - low)
+    return map_color(t, low_c, high_c)
 
 
 def agent_portrayal(agent):
     return AgentPortrayalStyle(
         x=agent.cell.coordinate[0],
         y=agent.cell.coordinate[1],
-        color="red",
+        color=num_to_color(agent.sugar, low=0, high=50),
         marker="o",
         size=10,
         zorder=1,
@@ -81,7 +131,7 @@ page = SolaraViz(
         make_plot_component("#Agents", page=1),
     ],
     model_params=model_params,
-    name="Sugarscape (Sugar Only)",
+    name="Sugarscape",
     play_interval=150,
 )
 
